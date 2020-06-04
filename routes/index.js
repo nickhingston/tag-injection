ca385a03d409777b00e3f1549b996a3b8669a45c
Nick Hingston <nick@wearemothership.com>
2020-06-04 12:53:58 +0100

    inject into actual webpage

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,31 @@ let express = require('express');
 let rp = require('request-promise')
 let router = express.Router();
 
+const apiUrl = "https://dev.vpop-pro.com/api/v1"
+const appUrl = "https://dev-app.vpop-pro.com/"
+
 /* GET home page. */
 router.get('/cases/:id', function(req, res, next) {
-  return rp(`https://dev.vpop-pro.com/api/v1/cases/${req.params.id}`)
-    .then((response) => {
-      // <meta property="og:title" content="<%= name %>" />
-      // <meta property="og:url" content="<%= url %>" />
-      // <meta property="og:description" content="<%= description %>" />
-      // <meta property="og:image" content="<%= thumbUrl %>" />
-      // <meta name="twitter:card" content="<%= thumbUrl %>" />
-      // <meta name="twitter:title" content="<%= name %>" />
-      // <meta name="twitter:description" content="<%= description %>" />
-      // <meta name="twitter:image" content="<%= thumbUrl %>" />
-      const json = JSON.parse(response);
-      res.render('index', { 
-        name: json.name,
-        url: `${req.protocol}://${req.get('host')}/cases/${req.params.id}`,
-        thumbUrl: `https://dev.vpop-pro.com/api/v1/xrays/${json.xrays[0].id}/thumb`,
-        description: `temp description!`      
-      });
-    })
+  return rp(appUrl)
+    .then(htmlPage => rp(`${apiUrl}/cases/${req.params.id}`)
+      .then((response) => {
+        const json = JSON.parse(response);
+
+        htmlPage.replace('<title>vPOP</title>',`         
+          <title>vPOP</title>
+          <meta property="og:title" content="${json.name}" />
+          <meta property="og:url" content="${json.url}" />
+          <meta property="og:description" content="${json.description}" />
+          <meta property="og:image" content="${json.thumbUrl}" />
+          <meta name="twitter:card" content="${json.thumbUrl}" />
+          <meta name="twitter:title" content="${json.name}" />
+          <meta name="twitter:description" content="${json.description}" />
+          <meta name="twitter:image" content="${json.thumbUrl}" />`);
+  
+        res.send(htmlPage);
+  
+      })
+    )
     .catch(next)
 });
 
