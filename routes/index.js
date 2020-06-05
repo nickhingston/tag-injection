d449eb60f154d69edf377ee318654f658ddf347c
Nick Hingston <nick@wearemothership.com>
2020-06-05 17:12:30 +0100

    make more robust, handle non public cases ok

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,53 @@
-let express = require('express');
-let rp = require('request-promise')
-let router = express.Router();
+const express = require('express');
+const rp = require('request-promise')
+const router = express.Router();
 
 const apiUrl = "https://dev.vpop-pro.com/api/v1"
 const appUrl = "https://dev-app.vpop-pro.com/"
 
+let htmlPage = ""
+
+const updatePage  = () => {
+  // just get page from 
+  rp(appUrl).then(response => htmlPage = response);
+}
+updatePage();
+setInterval(updatePage, 1000*60*30) // update the page every 30mins incase we change it
+
+
 /* GET home page. */
-router.get('/cases/:id', function(req, res, next) {
-  return rp(appUrl)
-    .then(htmlPage => rp(`${apiUrl}/cases/${req.params.id}`)
-      .then((response) => {
-        const json = JSON.parse(response);
+router.get('/cases/:id', (req, res, next) => {
+  rp({ uri:`${apiUrl}/cases/${req.params.id}`, simple: false, resolveWithFullResponse: true })
+    .then((response) => {
+      if (response.statusCode != 200) {
+        return res.send(htmlPage).end()
+      }
 
-        const tagData = {
-          name: json.name,
-          url: `${req.protocol}://${req.get('host')}/cases/${req.params.id}`,
-          thumbUrl: `${apiUrl}/xrays/${json.xrays[0].id}/thumb`,
-          description: `temp description!`   
-        };
+      const json = JSON.parse(response.body);
 
-        res.send(htmlPage.replace('<title>vPOP</title>',`         
-          <title>vPOP</title>
-          <meta property="og:title" content="${tagData.name}" />
-          <meta property="og:url" content="${tagData.url}" />
-          <meta property="og:description" content="${tagData.description}" />
-          <meta property="og:image" content="${tagData.thumbUrl}" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="${tagData.name}" />
-          <meta name="twitter:description" content="${tagData.description}" />
-          <meta name="twitter:image" content="${tagData.thumbUrl}" />`));
-  
-      })
-    )
-    .catch(next)
+      const tagData = {
+        name: json.name,
+        url: `${req.protocol}://${req.get('host')}/cases/${req.params.id}`,
+        thumbUrl: `${apiUrl}/xrays/${json.xrays[0].id}/thumb`,
+        description: `temp description!`   
+      };
+
+      res.send(htmlPage.replace('<title>vPOP</title>',`         
+        <title>vPOP</title>
+        <meta property="og:title" content="${tagData.name}" />
+        <meta property="og:url" content="${tagData.url}" />
+        <meta property="og:description" content="${tagData.description}" />
+        <meta property="og:image" content="${tagData.thumbUrl}" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="${tagData.name}" />
+        <meta name="twitter:description" content="${tagData.description}" />
+        <meta name="twitter:image" content="${tagData.thumbUrl}" />`));
+
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(htmlPage).end();
+    })
 });
 
 module.exports = router;
