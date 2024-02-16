b155a7dedcc913fcaa62e0654c0bee9df5c7fbf5
Nick <nick@wearemothership.com>
2024-02-16 18:39:12 +0000

    allow case paths with xray/info

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const rp = require('request-promise')
 const router = express.Router();
 
-const apiUrl = "https://vpop-pro.com/api/v1"
-const appUrl = "https://dev-app.vpop-pro.com/"
+const apiUrl = "https://dev.vpop-pro.com/api/v1"
+const appUrl = "https://rw-app.vpop-pro.com/"
 
 let htmlPage = ""
 
@@ -16,8 +16,9 @@ setInterval(updatePage, 1000*60*30) // update the page every 30mins incase we ch
 
 
 /* GET home page. */
-router.get('/cases/:id', (req, res, next) => {
-  rp({ uri:`${apiUrl}/cases/${req.params.id}`, simple: false, resolveWithFullResponse: true })
+router.get('/cases/:id/:id2?/:id3?', (req, res, next) => {
+  const frag = [req.params.id, req.params.id2, req.params.id3].filter((param) => !!param).join('/')
+  rp({ uri:`${apiUrl}/cases/${frag}`, simple: false, resolveWithFullResponse: true })
     .then((response) => {
       if (response.statusCode != 200) {
         return res.send(htmlPage).end()
@@ -28,17 +29,20 @@ router.get('/cases/:id', (req, res, next) => {
       const tagData = {
         name: json.name,
         url: `${appUrl}/cases/${req.params.id}`,
-        thumbUrl: `${apiUrl}/xrays/${json.xrays[0].id}/thumb`,
+        thumbUrl: `${apiUrl}/xrays/${json.xrays[0].id}/thumb.jpeg`,
         description: 'Made&nbsp;by&nbsp;VPOP&nbsp;PRO'
       };
 
       res.send(htmlPage.replace('<title>vPOP</title>',`         
         <title>vPOP</title>
-        <meta property="og:type" content="website" />
+	<meta name="description" content="Veterinary Preoperative Orthopaedic Planner" />
+	<meta property="fb:app_id" content="267530317797644" />
+	<meta property="og:description" content="Veterinary Preoperative Orthopaedic Planner" />      
+        <meta property="og:type" content="article" />
         <meta property="og:title" content="${tagData.name}" />
         <meta property="og:url" content="${tagData.url}" />
-        <meta property="og:image:secure_url" itemprop="image" content="${tagData.thumbUrl}" />
-        <meta name="twitter:card" content="summary_large_image" />
+	<meta property="og:image" itemprop="image" content="${tagData.thumbUrl}" />
+	<meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="${tagData.name}" />
         <meta name="twitter:description" content=“${tagData.description}" />
         <meta name="twitter:image" content="${tagData.thumbUrl}" />`
