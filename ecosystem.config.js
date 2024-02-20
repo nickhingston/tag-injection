7cb2515716fd383427a654a0382dc0d7a65a0e8a
Nick Hingston <nick@wearemothership.com>
2024-02-20 21:15:38 +0000

    fix ecosystem file - II

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   apps: [{
     name: 'tag-injection',
-    script: '.',
+    script: './bin/www',
     instances: 2,
     log_date_format: 'YYYY-MM-DDTHH:mm:ss.SSS',
     wait_ready: true,
     exec_mode: 'cluster',
     env_dev: {
       NODE_ENV: 'production',
-      API_URL: 'https://dev.vpop-pro.com',
+      API_URL: 'https://dev.vpop-pro.com/api/v1',
       APP_URL: 'https://dev-app.vpop-pro.com'
     },
     env_beta: {
       NODE_ENV: 'production',
-      API_URL: 'https://dev.vpop-pro.com',
+      API_URL: 'https://dev.vpop-pro.com/api/v1',
       APP_URL: 'https://beta-app.vpop-pro.com'
     },
     env_production: {
       NODE_ENV: 'production',
-      API_URL: 'https://dev.vpop-pro.com',
+      API_URL: 'https://dev.vpop-pro.com/api/v1',
       APP_URL: 'https://app.vpop-pro.com'
     }
   }],
@@ -31,7 +31,7 @@ module.exports = {
       repo: 'https://github.com/nickhingston/tag-injection.git',
       path: '/home/vpop/tag-injection',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production --update-env'
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev --update-env'
     },
     beta: {
       user: 'vpop',
@@ -40,7 +40,7 @@ module.exports = {
       repo: 'https://github.com/nickhingston/tag-injection.git',
       path: '/home/vpop/tag-injection',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production --update-env'
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env beta --update-env'
     },
     production: {
       user: 'vpop',
